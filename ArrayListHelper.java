36fb80de3e9d000d32f14a20b1527fbda0c012ea
Rami Sahyouni <rami.sahyouni@trulioo.com>
2020-03-24 21:43:33 -0700

    cleanup

--- ArrayListHelper.java
+++ ArrayListHelper.java
@@ -9,32 +9,19 @@ public class ArrayListHelper {
     }
 
     public void Sort() {
-        Collections.sort(this.arrayList);
     }
 
     public void Reverse() {
-        Collections.reverse(this.arrayList);
     }
 
     public void Append(ArrayList<Integer> list) {
 
-        for (int index = 0; index < list.size(); index++) {
-            this.arrayList.add(list.get(index));
-        }
+       
     }
 
     public String Display() {
 
-        String result = "";
-
-        for (int index = 0; index < this.arrayList.size(); index++) {
-            result += this.arrayList.get(index);
-
-            if (index != this.arrayList.size() - 1)
-                result += "-";
-        }
-
-        return result;
+       return "";
 
     }
 }
